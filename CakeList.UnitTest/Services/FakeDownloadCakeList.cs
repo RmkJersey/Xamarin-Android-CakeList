ed68efcf886ddc30c951ce79dd270bd8745984e7
Richard Kay <richard.kay@klass-online.com>
2020-10-06 18:19:15 +0100

    New fake classes added to unit test project
    1) FakeNetworkHelper
    2) FakeDownloadCakeList

---
+++ CakeList.UnitTest/Services/FakeDownloadCakeList.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using CakeList.Interfaces;
+using CakeList.Models;
+
+namespace CakeList.UnitTest.Services
+{
+    class FakeDownloadCakeList : IDownloadCakeList
+    {
+        public enum Mode
+        {
+            ReturnsNull, ReturnsList
+
+        }
+
+        private readonly Mode _mode;
+
+        public FakeDownloadCakeList(Mode mode)
+        {
+            _mode = mode;
+        }
+
+        public async Task<List<WaracleCake>> GetCakeList()
+        {
+            if (_mode == Mode.ReturnsNull)
+                return null;
+            
+            //Additional methods can be added here for other tests
+            ////Todo
+            //// Implement method to test successful list without the dependency on a network connection
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CakeList.UnitTest/Services/FakeNetworkHelper.cs
@@ -0,0 +1,23 @@
+﻿using Xamarin.Essentials;
+using CakeList.Interfaces;
+using CakeList.Services;
+
+namespace CakeList.UnitTest.Services
+{
+    public class FakeNetworkHelper : INetworkHelper
+    {
+        private readonly bool _fakeNetworkConnected;
+        private readonly string _fakeNetworkMessage;
+
+        public FakeNetworkHelper(NetworkAccess networkStatus)
+        {
+            _fakeNetworkConnected = new NetworkStatusMessage().Get(networkStatus, out _fakeNetworkMessage);
+        }
+
+        public bool CheckNetworkStatus(out string networkMessage)
+        {
+            networkMessage = _fakeNetworkMessage;
+            return _fakeNetworkConnected;
+        }
+    }
+}
