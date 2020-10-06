6585c77cd3e7d477f9b9f1ffdac54cb5f018d96a
Richard Kay <richard.kay@klass-online.com>
2020-10-06 15:06:36 +0100

    Initial commit of WaracleCakeList model class
    ////Todo
     Create functions to:
        1) Add range of cakes to public list
        2) Add sort function
        3) Add function to remove any duplicates from the list

---
+++ CakeList/Models/WaracleCakeList.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace CakeList.Models
+{
+    ////Todo
+    /// Create functions to
+    /// 1) Add range of cakes to list
+    /// 2) Add sort function
+    /// 3) Add function to remove any duplicates from the list
+    public class WaracleCakeList
+    {
+    }
+}
