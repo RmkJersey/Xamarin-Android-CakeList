30ea1f120c5502d779a573606c0fdf5a3ba08e58
Richard Kay <richard.kay@klass-online.com>
2020-10-06 17:18:05 +0100

    async method completed to manage donwloading of GetCakeList method

--- CakeList/Services/CakeListManager.cs
+++ CakeList/Services/CakeListManager.cs
@@ -1,10 +1,61 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using CakeList.Models;
+using System;
+using System.Collections.ObjectModel;
 
 namespace CakeList.Services
 {
-    public  class CakeListManager
+    /// <summary>
+    /// CakeListManager class
+    /// </summary>
+    public class CakeListManager
     {
+        public WaracleCakeList DownlodedCakeList = new WaracleCakeList();
+        public string ErrorMessage = string.Empty;
+        public bool Loaded;
+        public bool Loading;
+        private Action _doneFunc;
+
+        public async void DownLoadCakeList(Action done = null)
+        {
+            // Record call back function
+            _doneFunc = done;
+
+            // If we are already loading just return
+            if (Loading)
+                return;
+
+            // If we have already loaded the data just call the callback function and return
+            if (Loaded)
+            {
+                _doneFunc();
+                return;
+            }
+
+            // We are loading now
+            Loading = true;
+
+            try
+            {
+                DownlodedCakeList = await CakesHelper.GetCakeList();
+                Loaded = true;
+                Loading = false;
+
+                // if there is a callback function call it.
+                _doneFunc?.Invoke();
+            }
+            catch (Exception ex)
+            {
+                ErrorMessage = ex.Message;
+            }
+        }
+
+        public void Clear(ObservableCollection<WaracleCake> obeservableCakeList)
+        {
+            DownlodedCakeList.Cakes.Clear();
+            obeservableCakeList.Clear();
+            Loaded = false;
+            Loading = false;
+            ErrorMessage = "";
+        }
     }
 }
