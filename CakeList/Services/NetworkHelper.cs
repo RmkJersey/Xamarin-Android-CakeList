00324ac5522057b8a097e3886f8eabf1594ce423
Richard Kay <richard.kay@klass-online.com>
2020-10-06 16:08:07 +0100

    Network helper class added Annotations added

---
+++ CakeList/Services/NetworkHelper.cs
@@ -0,0 +1,19 @@
+﻿using CakeList.Interfaces;
+using Xamarin.Essentials;
+
+namespace CakeList.Services
+{
+    /// <summary>
+    /// Network helper class
+    /// </summary>
+    public class NetworkHelper : INetworkHelper
+    {
+        /// <param name="networkMessage"></param>
+        /// <returns><c>networkMessage</c></returns>
+        /// <returns><c>true</c> if the network <c>NetworkAccess</c> is equal to <c>NetworkAccess.Internet</c>; otherwise, <c>false></c></returns>
+        public bool CheckNetworkStatus(out string networkMessage)
+        {
+            return new NetworkStatusMessage().Get(Connectivity.NetworkAccess, out networkMessage);
+        }
+    }
+}
