a3316d793360a6c22b18143efd89b40b0c09c47c
Richard Kay <richard.kay@klass-online.com>
2020-10-06 16:14:56 +0100

    Initial commit of CakesHelper
    Todo:
      asynchronous method required to download remote cakelist
          Requirements:
              Create cake list from REST API response data
                  1) Removes any duplicate entries
                  2) Sort the list by title
                  3) Uppercase fist letter of title and desc

---
+++ CakeList/Services/CakesHelper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CakeList.Services
+{
+    public class CakesHelper
+    {
+        ////Todo
+        //// asynchronous method required to download remote cakelist
+        //// Requirements:
+        ////    Create list from REST API response data
+        ////        1) Removes any duplicate entries
+        ////        2) Sort the list by title
+        ////        3) Uppercase fist letter of title and desc
+    }
+}
