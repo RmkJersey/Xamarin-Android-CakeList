a403f1c7bd2812f5d767adc712204847fd1604f8
Richard Kay <richard.kay@klass-online.com>
2020-10-06 15:55:01 +0100

    Class added to return network status and relevant network message
    Will be used to display message to user if download of cake list is not available or access to the internet is unavailable (or both)
    
    Annotatons to be added

---
+++ CakeList/Services/NetworkStatusMessage.cs
@@ -0,0 +1,35 @@
+﻿using Xamarin.Essentials;
+
+namespace CakeList.Services
+{
+    public class NetworkStatusMessage
+    {
+        ////Todo
+        /// Add annotations 
+        public bool Get(NetworkAccess networkStatus, out string networkMessage)
+        {
+            string errorSuffixMessage = @", please check your connection and try again";
+            bool networkConnectionStatus;
+            switch (networkStatus)
+            {
+                case NetworkAccess.Internet:
+                    networkMessage = "Connected to internet";
+                    networkConnectionStatus = true;
+                    break;
+                case NetworkAccess.Local:
+                case NetworkAccess.ConstrainedInternet:
+                case NetworkAccess.None:
+                case NetworkAccess.Unknown:
+                    networkMessage = "No internet available" + errorSuffixMessage;
+                    networkConnectionStatus = false;
+                    break;
+                default:
+                    networkMessage = "Internet access is unknown" + errorSuffixMessage;
+                    networkConnectionStatus = false;
+                    break;
+            }
+
+            return networkConnectionStatus;
+        }
+    }
+}
