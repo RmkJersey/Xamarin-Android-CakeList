07da56498adff52173200062d54eadf29bf8e8f2
Richard Kay <richard.kay@klass-online.com>
2020-10-06 16:49:49 +0100

    async method completed to fetch cake list

--- CakeList/Services/DownloadCakeList.cs
+++ CakeList/Services/DownloadCakeList.cs
@@ -1,9 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Threading.Tasks;
-using CakeList.Interfaces;
+﻿using CakeList.Interfaces;
 using CakeList.Models;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace CakeList.Services
 {
@@ -12,10 +12,28 @@ namespace CakeList.Services
     /// </summary>
     public class DownloadCakeList : IDownloadCakeList
     {
-        /// <returns><c>Task WaracleCakeList</c></returns>
+        /// <returns><c>WaracleCake list</c></returns>
         public async Task<List<WaracleCake>> GetCakeList()
         {
-            return null;
+            IRestClient client = new RestClient("https://gist.githubusercontent.com/t-reed/739df99e9d96700f17604a3971e701fa/raw/1d4dd9c5a0ec758ff5ae92b7b13fe4d57d34e1dc");
+
+            IRestRequest request = new RestRequest("waracle_cake-android-client", Method.GET)
+            {
+                OnBeforeDeserialization = resp => { resp.ContentType = "application/json"; }
+            };
+
+            var response = await client.ExecuteAsync<List<WaracleCake>>(request);
+
+            if (response.IsSuccessful)
+                return response.Data;
+
+            var errorMessage = response.ErrorException.Message;
+            if (string.Equals(errorMessage, "Unable to read data from the transport connection: Connection reset by peer.", StringComparison.CurrentCultureIgnoreCase))
+            {
+                errorMessage = "Please check your network connection and try again!";
+            }
+
+            throw new Exception(errorMessage);
         }
     }
 }
