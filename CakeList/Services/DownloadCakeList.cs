fb33139af3904a3b07aa9302c8571f2932a3c6d2
Richard Kay <richard.kay@klass-online.com>
2020-10-06 16:33:12 +0100

    Initial commit for DownloadCakeList class

---
+++ CakeList/Services/DownloadCakeList.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using CakeList.Interfaces;
+using CakeList.Models;
+
+namespace CakeList.Services
+{
+    /// <summary>
+    /// Cake list download class
+    /// </summary>
+    public class DownloadCakeList : IDownloadCakeList
+    {
+        /// <returns><c>Task WaracleCakeList</c></returns>
+        public async Task<List<WaracleCake>> GetCakeList()
+        {
+            return null;
+        }
+    }
+}
