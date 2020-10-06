242f293c48a9664c290e6474922e6f259fd4bb4b
Richard Kay <richard.kay@klass-online.com>
2020-10-06 15:00:59 +0100

    WaracleCake model created, all annotations added
    FormatString class added, which performs the cleaning of title and description, removing whitespace and uppercase of 1st char of string

---
+++ CakeList/Models/WaracleCake.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using CakeList.Utility;
+
+namespace CakeList.Models
+{
+    /// <summary>
+    /// The main WaracleCake class
+    /// </summary>
+    public class WaracleCake : IEquatable<WaracleCake>
+    {
+        /// <summary>
+        /// Required for JSON deserialization
+        /// </summary>
+        public WaracleCake()
+        {
+        }
+
+        /// <summary>
+        /// Cake title
+        /// </summary>
+        public string Title { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Cake description
+        /// </summary>
+        public string Desc { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Cake image url
+        /// </summary>
+        public string Image { get; set; } = string.Empty;
+
+        /// <summary>Constructor to create each cake object</summary>
+        /// <param name="title">string</param>
+        /// <param name="description">string</param>
+        /// <param name="imageUrl">string</param>
+        /// <returns><c>WaracleCake</c></returns>
+        public WaracleCake(string title, string description, string imageUrl)
+        {
+            Title = FormatString.FirstCharToUpperTrimWhiteSpace(title);
+            Desc = FormatString.FirstCharToUpperTrimWhiteSpace(description);
+            Image = imageUrl;
+        }
+
+        /// <summary>Cleans title and description</summary>
+        /// <returns>cleaned <c>WaracleCake</c> object</returns>
+        public WaracleCake CleanTitleAndDesc()
+        {
+            Title = FormatString.FirstCharToUpperTrimWhiteSpace(Title);
+            Desc = FormatString.FirstCharToUpperTrimWhiteSpace(Desc);
+
+            return this;
+        }
+
+        /// <summary>Determines whether the specified <c>WaracleCake</c> is equal to the current <c>WaracleCake</c> using the <c>Title</c> field.</summary>
+        /// <param name="other">The object to compare with the current <c>WaracleCake</c>.</param>
+        /// <returns><c>true</c> if the specified <c>WaracleCake.Title</c> is equal to the current <c>WaracleCake.Title</c>; otherwise, <c>false></c></returns>
+        public bool Equals(WaracleCake other)
+        {
+            return string.Equals(Title, other.Title, StringComparison.InvariantCultureIgnoreCase);
+        }
+
+        public override int GetHashCode()
+        {
+            return Title.ToLowerInvariant().GetHashCode();
+        }
+    }
+}
---
+++ CakeList/Utility/FormatString.cs
@@ -0,0 +1,28 @@
+﻿namespace CakeList.Utility
+{
+    public class FormatString
+    {
+        /// <summary>Cleans up the input string removing leading and trailing edges, and upper cases first letter</summary>
+        /// <param name="stringInput">string</param>
+        /// <returns>Newly formatted <c>string</c></returns>
+        public static string FirstCharToUpperTrimWhiteSpace(string stringInput)
+        {
+            return FirstCharToUpper(stringInput.Trim());
+        }
+
+        /// <summary>Upper case first letter of string</summary>
+        /// <param name="stringInput">string</param>
+        /// <returns>Upper cased first word of <c>string</c></returns>
+        public static string FirstCharToUpper(string stringInput)
+        {
+            // Check for empty string
+            if (string.IsNullOrEmpty(stringInput))
+            {
+                return string.Empty;
+            }
+
+            // Return 1st char uppercase and concat remainder substring
+            return char.ToUpper(stringInput[0]) + stringInput.Substring(1);
+        }
+    }
+}
